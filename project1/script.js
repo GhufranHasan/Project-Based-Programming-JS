28347ae702430704f5c3fdc5b27ce0f2964909ce
Ghufran Hasan <ghufran.aims@gmail.com>
2022-12-06 17:03:40 +0500

    Project1

---
+++ project1/script.js
@@ -0,0 +1,24 @@
+// This is the main JS for Project 1
+
+// const userName = prompt("Hi My name is Ghufran. What's your name?");
+
+// console.log(userName);
+
+
+// Bad Code
+var X = "Imran";
+
+// Good Code
+
+console.log("Before creating variable", teacherName);
+
+var teacherName = "Imran";
+
+console.log("After creating variable", teacherName);
+
+var subject = "JavaScript";
+
+teacherName = "Alexander";
+
+// console.log("After reassigning variable", teacherName);
+console.log("After going to the US", teacherName);
---
+++ project1/script2.js
@@ -0,0 +1,15 @@
+let teacherName2 = "Alexander";
+console.log(typeof teacherName2);
+
+let teacherAge = 18;
+console.log(typeof teacherAge);
+
+let nameChanged = true;
+console.log(typeof nameChanged);
+
+let teacherClass = undefined; // OR
+// let teacherClass;
+console.log(typeof teacherClass);
+
+let teacherFavouriteStudent = null;
+console.log(typeof teacherFavouriteStudent);
