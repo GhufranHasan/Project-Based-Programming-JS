fc6ddef6f54136bb047587b316d71d3cb85f2969
Ghufran Hasan <ghufran.aims@gmail.com>
2022-12-06 17:03:30 +0500

    Project0

---
+++ project0/script.js
@@ -0,0 +1,8 @@
+// alert('Hello World');
+
+// Show an alert to the viewer.
+alert('Hello World from Javascript!');
+
+console.log("Hello Console from Javascript!");
+
+console.log(prompt("What is your name?"));
